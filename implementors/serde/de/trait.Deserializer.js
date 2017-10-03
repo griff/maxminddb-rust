4b812c47cbcb53d06617b445b39092735b406a88
Travis CI User <travis@example.org>
2017-10-03 03:37:37 +0000

    Documentation for oschwald/maxminddb-rust@f587889

---
+++ implementors/serde/de/trait.Deserializer.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["maxminddb"] = ["impl&lt;'de, 'a&gt; <a class=\"trait\" href=\"serde/de/trait.Deserializer.html\" title=\"trait serde::de::Deserializer\">Deserializer</a>&lt;'de&gt; for &amp;'a mut Decoder",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
